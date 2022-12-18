da1abf8042ff4a9f78e8be0b21040f7d6977065d
farjunShiran <devshiranf@gmail.com>
2022-12-18 12:05:14 +0200

    getRooms

--- frontend/src/app/services/chat.service.ts
+++ frontend/src/app/services/chat.service.ts
@@ -1,11 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
+import { IChatRoom } from '../models';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
+  constructor(private _db: AngularFirestore) {}
 
-  constructor() { }
-
-  
+  getRooms(): Observable<Array<IChatRoom>> {
+    return this._db
+      .collection('rooms')
+      .snapshotChanges()
+      .pipe(
+        map((snaps) => {
+          return snaps.map((snaps) => {
+            const id = snaps.payload.doc.id;
+            const data: IChatRoom = <IChatRoom>snaps.payload.doc.data();
+            return <IChatRoom>{
+              ...data,
+              id
+            };
+          });
+        })
+      );
+  }
 }
